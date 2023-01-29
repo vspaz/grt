574e3300fef4eef5b3db3e3e02527080852ac399
vspaz <vladimir.edelman@gmail.com>
2023-01-29 20:11:49 +0100

    add Run method

--- app/app.go
+++ app/app.go
@@ -1,1 +1,13 @@
 package app
+
+import (
+	"github.com/vspaz/simplelogger/pkg/logging"
+	"gtf/cmd"
+	"os"
+)
+
+func Run() {
+	args := cmd.GetCmdArguments(os.Args)
+	logger := logging.GetTextLogger(args.LogLevel).Logger
+	logger.Info("app started")
+}
