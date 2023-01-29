9e30d9c4d7c07294ec2f31cb29dc7fdd84f6eeca
vspaz <vladimir.edelman@gmail.com>
2023-01-29 20:25:52 +0100

    add basic config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"github.com/go-redis/redis/v8"
+	"sync"
+)
+
+var (
+	config SingletonConfig
+	once   sync.Once
+)
+
+type SingletonConfig struct {
+	Config *Conf
+}
+
+type Conf struct {
+	Redis *redis.Options
+}
+
+func initConfig() SingletonConfig {
+	return SingletonConfig{}
+}
+
+func GetGlobalConfig() SingletonConfig {
+	once.Do(
+		func() {
+			config = initConfig()
+		},
+	)
+	return config
+}
--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.19
 
 require (
 	github.com/go-chi/render v1.0.2
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/sirupsen/logrus v1.9.0
 	github.com/vspaz/simplelogger v0.0.1
 )
 
 require (
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	golang.org/x/sys v0.3.0 // indirect
 )
