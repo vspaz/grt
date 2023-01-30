be532a77704d8fdd1bedbc8f9a28f033112407ae
vspaz <vladimir.edelman@gmail.com>
2023-01-30 18:28:13 +0100

    add a handler stub

--- handlers/router.go
+++ handlers/router.go
@@ -1,13 +1,25 @@
 package handlers
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
 )
 
 type Router struct {
 	Logger *logrus.Logger
+}
+
+func (r *Router) Get(response http.ResponseWriter, request *http.Request) {
+	id := chi.URLParam(request, "id")
+	okBody, _ := json.Marshal(map[string]string{id: "ok"})
+	response.WriteHeader(http.StatusOK)
+	response.Header().Set("Content-Type", "application/json")
+	response.Header().Set("Content-Length", strconv.Itoa(len(okBody)))
+	response.Write(okBody)
 }
 
 func RegisterHandlers(mux *chi.Mux, handlers *Router) *chi.Mux {
