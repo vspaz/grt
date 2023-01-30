b89ccc0da5a327fc32f809405e27ef4b1098baa4
vspaz <vladimir.edelman@gmail.com>
2023-01-30 18:05:33 +0100

    add RegisterHandlers method

--- handlers/router.go
+++ handlers/router.go
@@ -1,7 +1,16 @@
 package handlers
 
-import "github.com/sirupsen/logrus"
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/sirupsen/logrus"
+)
 
 type Router struct {
 	Logger *logrus.Logger
+}
+
+func RegisterHandlers(mux *chi.Mux, handlers *Router) *chi.Mux {
+	// apiV1Prefix := "/api/v1/"
+	mux.Get("/ping/", Router{}.GetHealthStatus)
+	return mux
 }
