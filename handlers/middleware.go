59d501fb925db6888c6f46f14358ff87ed0e77c8
vspaz <vladimir.edelman@gmail.com>
2023-01-30 18:00:35 +0100

    add ConfigureMiddleware method

--- handlers/middleware.go
+++ handlers/middleware.go
@@ -3,11 +3,16 @@ package handlers
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 	"github.com/sirupsen/logrus"
 )
 
-func ConfigureMiddleware(logger *logrus.Logger) *chi.Mux {
+func ConfigureMiddleware(log *logrus.Logger) *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Recoverer)
+	router.Use(middleware.RealIP)
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Heartbeat("/ping"))
+	router.Use(render.SetContentType(render.ContentTypeJSON))
 	return router
 }
